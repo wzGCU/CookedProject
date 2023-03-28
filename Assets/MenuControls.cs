92583c0d5a4658fbe0231f056851d5cebd31b183
Zbordek - Wiktor Zborowski <61721211+wzGCU@users.noreply.github.com>
2023-03-28 03:10:02 +0100

    repackaging

---
+++ Assets/MenuControls.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager;
+
+
+namespace Undercooked
+{
+    public class MenuControls : MonoBehaviour
+    {
+       
+        void OpenMapA()
+        {
+        
+        }
+
+        
+        void OpenMapB()
+        {
+         SceneManager
+        }
+    }
+}
